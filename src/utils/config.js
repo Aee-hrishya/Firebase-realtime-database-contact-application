594852aff9fa8fc455145dcc767084a3a7034538
hrishikesh <hshinde166@gmail.com>
2022-01-07 01:17:34 +0530

    This is the config file for the image that we want to compress

---
+++ src/utils/config.js
@@ -0,0 +1,9 @@
+//TODO: add firebase configuration and export it
+
+//image configuration to compress the images
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
