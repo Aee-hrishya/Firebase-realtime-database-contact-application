8483368731b10d207e73be7c44585d254ff17c17
hrishikesh <hshinde166@gmail.com>
2022-01-08 02:30:51 +0530

    footer for the application

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-info text-center pt-3 pb-2 mt-5 sticky-bottom">
-      A simple Contact App
+      A Contact App
     </footer>
   );
 };
