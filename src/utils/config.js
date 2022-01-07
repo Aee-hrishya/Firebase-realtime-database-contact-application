6cbdec1c023699b9c0b6f8d8df35dfe147b84a3e
hrishikesh <hshinde166@gmail.com>
2022-01-08 02:28:57 +0530

    Firebase config files

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,15 @@
 //TODO: add firebase configuration and export it
+ export const firebaseConfig = {
+  apiKey: "AIzaSyDa4QLgU47c3mc14uJKSFF_dSOP7-HakUo",
+  authDomain: "contacts-realtime-database.firebaseapp.com",
+  databaseURL: "https://contacts-realtime-database-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "contacts-realtime-database",
+  storageBucket: "contacts-realtime-database.appspot.com",
+  messagingSenderId: "319706171403",
+  appId: "1:319706171403:web:608f63851bb6bc219a5f0e",
+  measurementId: "G-6F5BN48HXW"
+}
+
 
 //image configuration to compress the images
 export const imageConfig = {
