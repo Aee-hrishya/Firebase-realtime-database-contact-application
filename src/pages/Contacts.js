61c1690c72a7b51034713409ff5526fe9482ead8
hrishikesh <hshinde166@gmail.com>
2022-01-10 13:29:58 +0530

    Logic to delete and update the contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -4,7 +4,7 @@ import { Container, ListGroup, ListGroupItem, Spinner } from "reactstrap";
 import Contact from "../components/Contact";
 import { MdAdd } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-import { ContactContext } from "../context/Context";
+import ContactContext  from "../context/Context";
 import { CONTACT_TO_UPDATE } from "../context/action.types";
 
 const Contacts = () => {
@@ -19,7 +19,13 @@ const Contacts = () => {
   // handle fab icon button click
   // will set in state of the contact to update and send it to the contact/add route
   const AddContact = () => {
-    //TODO: use dispatch to send user to add contact screen
+    
+    //We created this dispatch because we didnt want the user to manually go to /contact/add and update the page so we took this precaution
+    dispatch({
+      type: CONTACT_TO_UPDATE,
+      payload: null,
+      key: null
+    })
     navigate("/contact/add");
   };
 
@@ -35,7 +41,21 @@ const Contacts = () => {
 
   return (
     <Container className="mt-4">
-      {/* TODO: Loop through FIREBASE objects  */}
+      
+      {contacts.length === 0 && !isLoading ? (
+        <div className="Center text-large text-primary"> No contacts found in firebase</div>
+      ) : (
+        <ListGroup>
+          {/*Object.entries is a special javascript method to loop through objects and it returns an array which we can loop through */}
+          {
+            Object.entries(contacts).map(([key, value]) => (
+              <ListGroupItem key={key}>
+                <Contact contact={value} contactKey={key} />
+              </ListGroupItem>
+            ))
+          }
+        </ListGroup>
+      )}
       <MdAdd className="fab icon " onClick={AddContact} />
     </Container>
   );
