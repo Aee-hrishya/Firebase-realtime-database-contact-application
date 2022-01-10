1944fa4030327f9d2802d46ea9898239666dbf93
hrishikesh <hshinde166@gmail.com>
2022-01-10 13:27:59 +0530

    Logic to get contacts from the firebase

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ import "./App.css";
 
 // firebase stuffs
 //TODO: import firebase config and firebase database
+import { firebaseConfig } from "./utils/config";
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/storage";
+
 
 // components
 import AddContact from "./pages/AddContact";
@@ -26,9 +31,13 @@ import PageNotFound from "./pages/PageNotFound";
 
 // context api stuffs
 //TODO: import reducers and contexts
+import reducer from "./context/reducer";
+import ContactContext from "./context/Context";
+import { SET_CONTACT, SET_LOADING } from "./context/action.types";
 
 //initlizeing firebase app with the firebase config which are in ./utils/firebaseConfig
 //TODO: initialize FIREBASE
+firebase.initializeApp(firebaseConfig);
 
 // first state to provide in react reducer
 const initialState = {
@@ -40,22 +49,45 @@ const initialState = {
 };
 
 const App = () => {
+
+
   const [state, dispatch] = useReducer(reducer, initialState);
 
   // will get contacts from firebase and set it on state contacts array
   const getContacts = async () => {
-    // TODO: load existing data
+    // load existing data
+    dispatch({
+      type:SET_LOADING, //This will show a spinner when data is loading
+      payload: true
+    });
+
+    //Now we are getting the info from the database
+    const contactsRef = await firebase.database().ref("/contacts");
+
+    //Firing the onValue event of firebase to get the data from the firebase database
+    contactsRef.on("value", snapshot => {
+      dispatch({
+        type: SET_CONTACT,
+        payload: snapshot.val()
+      });
+
+      dispatch({
+        type:SET_LOADING, //This will turn of the spinner after the contacts are loaded
+        payload: false
+      }); 
+
+    });
   };
 
   // getting contact  when component did mount
   useEffect(() => {
     //FIXME: call methods if needed
+    getContacts();
   }, []);
 
   return (
     <Router>
-      {/* FIXME: Provider is not configured */}
-      <ContactContext.Provider>
+      <ContactContext.Provider value={{state, dispatch}}>
         <ToastContainer />
         <Header />
         <Container>
